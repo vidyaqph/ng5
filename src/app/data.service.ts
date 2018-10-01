c968889ec10073aa1361a7a3741ae26df8c74672
DB\madhusudananv <vidya.madhusudanan@qpharmacorp.com>
2018-10-01 12:29:34 -0400

    first commit

---
+++ src/app/data.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import {BehaviorSubject} from 'rxjs/BehaviorSubject'
+
+@Injectable()
+export class DataService {
+private goals = new BehaviorSubject<any>(['The initial goal', 'Another goal']);
+goal = this.goals.asObservable();
+  constructor() { }
+ChangeGoal(goal)
+{
+  this.goals.next(goal);
+}
+}
